713d62b906ea7b951714d4767b3ee5b4d1a996dc
Karim <layeche.torki@gmail.com>
2022-09-10 17:52:19 +0200

    add Folders

---
+++ RabbitApp/Sender.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitApp
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "Starting RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Message sent!{0}", message);
+            }
+        }
+    }
+}
---
+++ RabbitMQClient/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQClient
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            dynamic body;
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("received a message:{0}", message);
+                };
+                channel.BasicConsume("BasicTest", true, consumer);
+
+                Console.ReadLine();
+             
+             
+            }
+        }
+    }
+}
