6bc7a4cd21ae665579476771c69b825e359cf295
Karim <[meraga.karim@gmail.com]>
2022-09-11 22:03:33 +0200

    Init DependencyContainer

---
+++ RabbitAppSolution.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitAppSolution.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitAppSolution.Infra.IOC
+{
+    class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //our domai  bus
+            service.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
